0aaa75d46e9af0f40d7f145aec42acb18e3f6d8b
Borna Maticic <bornamaticic@yahoo.com>
2018-10-31 15:59:19 +0000

    completion of MVP

--- db/console.rb
+++ db/console.rb
@@ -2,9 +2,21 @@ require("pry")
 require_relative("../models/artist")
 require_relative("../models/album")
 
+
+
 artist1 = Artist.new("name"=>"David Bowie")
 artist1.save()
-p artist1
+artist2 = Artist.new("name"=>"Prince")
+artist2.save()
+# artist1
+p artist2
 
 album1 = Album.new({"title"=>"The Best of David Bowie", "genre" => "rock", 'artist_id' => artist1.id})
-p album1.save
+album1.save()
+album2 = Album.new({"title"=>"best of Prince", "genre" => "alt", 'artist_id' => artist2.id})
+album2.save()
+
+Album.all()
+Artist.all()
+Album.find_by_artist(artist1.id)
+p Artist.find_by_album(album2.id)
